27594cd63b9fd0d10f19d564fae811f74c5d4433
chpmobile <papc2_12@uni.worc.ac.uk>
2016-03-10 17:34:31 +0000

    change
    10/3/16

--- www/js/registration.js
+++ www/js/registration.js
@@ -6,23 +6,76 @@ var name = $("#name").val();
 var email = $("#email").val();
 var password = $("#password").val();
 var cpassword = $("#cpassword").val();
+
+createInput(name);
+
 if (name == '' || email == '' || password == '' || cpassword == '') {
-alert("Please fill all fields...!!!!!!");
+
+	alert("Please fill all fields...!!!!!!");
+
 } else if ((password.length) < 8) {
-alert("Password should atleast 8 character in length...!!!!!!");
+
+	alert("Password should atleast 8 character in length...!!!!!!");
+
 } else if (!(password).match(cpassword)) {
-alert("Your passwords don't match. Try again?");
+
+	alert("Your passwords don't match. Try again?");
+
 } else {
-$.post("register.php", {
-name1: name,
-email1: email,
-password1: password
-}, function(data) {
-if (data == 'You have Successfully Registered.....') {
-$("form")[0].reset();
+
+	$.post("php/register.php", {
+	name1: name,
+	email1: email,
+	password1: password
+	}, function(data) {
+
+	if (data == 'You have Successfully Registered.....') {
+
+		$("form")[0].reset();
+
+	}
+
+	alert(data);
+
+	});
+
 }
-alert(data);
+
+
+
 });
-}
-});
+
+
+
+// $('#addbutton').click(function(){
+
+// createInput();
+// test();
+
+
+
+// })
+
+// function createInput(){
+
+// 	    var $input = $('<input type="button" value="new button"/>');
+// 	    $input.appendTo($("body"));
+
+// 	}
+
+
+// 		function test(){
+
+
+// 			var r = $('<input/>').attr({
+
+// 				type:"button",
+// 				id: "field"
+
+// 			});
+
+// 			$("body").append(r);
+
+// 		}
+
 });
--- www/js/style.js
+++ www/js/style.js
@@ -1,6 +1,25 @@
+
 $(document).ready(function(){
-choosecolour();
-var $colour = $('.header').css('background').val();
+	choosecolour();
+
+	var $colour = $('.header').css('background').val();
+$("p").append("Some appended text."); 
+
+	$('#addbutton').click(function(){
+
+		
+
+		var r = $('<input/>').attr({
+
+			type:"button",
+			id: "field"
+
+		});
+
+		$('r').append('.addfields');
+
+
+	})
 
 });
 
@@ -37,5 +56,30 @@ $('.yellow').click(function(){
     $('.header').css('background','#ffd700');
 
 });
+
+}
+
+
+
+
+// function createInput(){
+
+// 	    var $input = $('<input type="button" value="new button"/>');
+// 	    $input.appendTo($("body"));
+
+// 	}
+
+
+function test(){
+
+
+	var r = $('<input/>').attr({
+
+		type:"button",
+		id: "field"
+
+	});
+
+	$('r').after('#addbutton');
 
 }
