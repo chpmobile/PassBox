4c5499b3ba1f817caff30ce0968df61eb0314c15
chpmobile <papc2_12@uni.worc.ac.uk>
2016-04-28 17:40:17 +0100

    n
    jkh

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,7 @@ $(document).on('pageinit', function() {
 function createMessage(){		
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'An example message.', duration: 2000}); 	
+    new Toast({content: 'Fill all the field please', duration: 2000}); 	
 }
    
      
